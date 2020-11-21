ab19163f1c0f7fbfa43b22b2d6a83fbe1f6d9ea0
DianArpian <>
2020-11-22 06:17:48 +0700

    TambahKurang

---
+++ src/pbo10/pkg10119916/latihan58/tambahkurang/PBO1010119916Latihan58TambahKurang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10.pkg10119916.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * NIM      : 10119916
+ * KELAS    : IF10K
+ */
+public class PBO1010119916Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JumlahBilangan jb = new JumlahBilangan(3, 4);
+        jb.tampilHasilJumlah();
+        SelisihBilangan sb= new SelisihBilangan(3, 4);
+        sb.tampilSelisih();
+    }
+    
+}
